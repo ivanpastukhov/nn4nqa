27ceb2c99f73758b289ce5ddfe04bb601f9f9bcd
ivanpastukhov <Ivan.shepherd@yandex.ru>
2018-12-27 08:23:32 +0000

    prelim results obtained

--- custom_model/train.py
+++ custom_model/train.py
@@ -9,7 +9,7 @@ class Train():
         self.losses = []
         pass
 
-    def fit(self, X_train_q, X_train_a,  y_train, model, batch_size, epochs, loss_function, optimizer):
+    def fit(self, X_train_q, X_train_a,  y_train, model, batch_size, epochs, loss_function, optimizer, device):
         assert len(X_train_q) == len(y_train) == len(X_train_a)
         len_dataset = len(X_train_a)
         step = 0
@@ -17,11 +17,11 @@ class Train():
             lb = 0
             rb = batch_size
             while rb <= len_dataset:
-                x_q_batch = X_train_q[lb:rb]
-                x_a_batch = X_train_a[lb:rb]
-                y_train_batch = y_train[lb:rb]
+                x_q_batch = X_train_q[lb:rb].to(device)
+                x_a_batch = X_train_a[lb:rb].to(device)
+                y_train_batch = y_train[lb:rb].to(device)
                 y_pred_batch = model(x_q_batch, x_a_batch)
-                loss = loss_function(y_pred_batch, y_train_batch)
+                loss = loss_function(y_pred_batch, y_train_batch).to(device)
                 self.losses.append(loss.item())
                 optimizer.zero_grad()
                 loss.backward()
@@ -30,3 +30,4 @@ class Train():
                 lb += batch_size
                 step += 1
                 self.steps.append(step)
+            print('Epoch: {}, loss: {}'.format(epoch, loss))
