f6a15e55d3aebd08ee27e7b22b78f3289d5ac565
Пастухов Иван <ISPastukhov@domclick.rucd>
2019-01-17 18:27:43 +0300

    softmax->cos_similarity

--- custom_model/model.py
+++ custom_model/model.py
@@ -418,7 +418,7 @@ class CrossAttentionNet(SimpleNet):
         right = torch.sum(outputs_r, dim=1)
         right = self.l_linear(right)
         print(left.size(), right.size())
-        ans = self.similarity(left, right).unsqueeze()
+        ans = self.similarity(left, right).unsqueeze(1)
         print(ans.size())
         ans = self.final_layer(ans)
         ans = F.softmax(ans, dim=1)
